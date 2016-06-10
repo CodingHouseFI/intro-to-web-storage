eb50eff180a755a8cc214c0fd42c19b2c6b51c57
Cade Nichols <cade@cadenichols.com>
2016-06-10 14:53:46 -0700

    edit area

--- main.js
+++ main.js
@@ -3,8 +3,46 @@ $(document).ready(init);
 
 function init() {
   $('.addName').click(addName);
+
   var names = getNames();  // read and parse
   renderNames(names);
+
+  $('.names').on('dblclick', 'li', deleteName);
+  $('.names').on('click', '.edit', editName);
+  // $('.names').on('click', '.save', saveEdit);
+  
+  $('.cancel').on('click', cancelEdit);
+}
+
+function cancelEdit() {
+  $('.editArea').hide();
+  $('.editName').val('');
+}
+
+
+function editName() {
+   // show the editArea
+
+  var name = $(this).siblings('.name').text();
+  var index = $(this).parent().index();
+
+  $('.editArea').data('editIndex', index);
+
+  $('.editName').val(name)
+  $('.editArea').show();
+}
+
+
+function deleteName(event) {
+  var name = $(this).text();
+  var index = $(this).index();
+
+  var names = getNames();
+
+  names.splice(index, 1);
+
+  writeNames(names);  // stringify, write
+  renderNames(names); // re-render DOM 
 }
 
 function addName() {
@@ -44,11 +82,15 @@ function renderNames(names) {
   // empty the list
   // append elements to list
 
-  var $lis = names.map(name => $('<li>').text(name));
+  var $lis = names.map(name => {
+    var $li = $('.template').clone();
+    $li.removeClass('template');
+    $li.find('.name').text(name);
+
+    return $li;
+  });
   $('ul.names').empty().append($lis);
 }
-
-
 
 
 
